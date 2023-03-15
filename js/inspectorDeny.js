6334b16e7d873ed1be0af3dc0fb05b39423f0aec
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2023-03-15 19:56:21 +0100

    removedSomeThingIDidNotNeed

--- js/inspectorDeny.js
+++ js/inspectorDeny.js
@@ -1,4 +1,1 @@
-// i want it to make it so if i open devtools then i go to a error pag
-function detectDevTool(allow) {
-    console.log("inspector");
-}
+// i want it to make it so if i open devtools then i go to a error page
